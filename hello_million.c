520a71140836877b8d1fe8613c8eec7d04585dd5
kitopito <kitopitowada@gmail.com>
2026-08-17 01:09:50 +0900

    hello2

--- hello_million.c
+++ hello_million.c
@@ -1,45 +1,9 @@
-
-
 #include <stdio.h>
-
-// void	f(void)
-// {
-// 	printf("Hello, World!\n");
-// 	return ;
-// }
-// void	hello(void)
-// {
-// 	int	a[1] = {0};
-
-// 	//	int	*addr[1] = {0};
-// 	// static int	i = 0;
-// 	// i++;
-// 	// printf("Hello, World! %p\n", hello);
-// 	// printf("Hello, World! %p\n", addr);
-// 	// printf("Hello, World! %p\n", &i);
-// 	// addr[2] = &f;
-// 	a[2] = &f;
-// 	return ;
-// }
-
-// int	main(void)
-// {
-// 	hello();
-// 	return (0);
-// }
-#include <stdio.h>
-
-// void	hack(void)
-// {
-// 	printf("Hacked!\n");
-// }
 
 void	func(void)
 {
 	int	*a[1];
 
-	// a[2] = hack;
-	// a[3] = (int *)hack;
 	printf("Hello, World!\n");
 	a[2] = (int *)func;
 	a[3] = (int *)func;
